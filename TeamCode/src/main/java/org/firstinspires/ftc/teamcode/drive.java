39f95b237411667244abc6cb4156226bdb5c93a7
STMA Robotics <robotics@stmarobotics.org>
2025-09-25 18:02:14 -0500

    Add starter bot code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ * This class represents a basic TeleOp program for a Mecanum drive robot.
+ * It's structured as a LinearOpMode, which means the code runs sequentially from top to bottom
+ * after the "INIT" button is pressed. [17]
+ */
+@TeleOp(name = "Mecanum Drive", group = "Drive")
+public class drive extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialization
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        if (isStopRequested()) return;
+        waitForStart();
+        while (opModeIsActive()) {
+            // Get gamepad input
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+            // Set motor powers
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
+
+        }
+    }
+}
