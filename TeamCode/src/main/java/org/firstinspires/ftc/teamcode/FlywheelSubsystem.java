e69291f6e391f6384be208f2a65a0cb9c17b62f7
STMA Robotics <robotics@stmarobotics.org>
2025-09-29 19:17:22 -0500

    Add subsystem and opmode code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelSubsystem.java
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class FlywheelSubsystem {
+
+    private final DcMotor flywheel;
+
+    public FlywheelSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
+        flywheel = hardwareMap.get(DcMotor.class, "flywheel");
+
+        flywheel.setDirection(DcMotor.Direction.FORWARD);
+        flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        flywheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void run(double power) {
+        flywheel.setPower(power);
+    }
+
+    public void stop() {
+        run(0);
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
@@ -1,4 +1,66 @@
 package org.firstinspires.ftc.teamcode;
 
-public class TeleopOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TeleopOpMode extends OpMode {
+
+    private DrivetrainSubsystem drivetrainSubsystem;
+    private FlywheelSubsystem flywheelSubsystem;
+    private IndexerSubsystem indexerSubsystem;
+
+    private boolean spinFlywheel = false;
+
+    @Override
+    public void init() {
+        drivetrainSubsystem = new DrivetrainSubsystem(hardwareMap, telemetry);
+        flywheelSubsystem = new FlywheelSubsystem(hardwareMap, telemetry);
+        indexerSubsystem = new IndexerSubsystem(hardwareMap, telemetry);
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+        drivetrainSubsystem.resetYaw();
+    }
+
+    @Override
+    public void loop() {
+        double translationY = Math.copySign(Math.pow(-gamepad1.left_stick_y, 2), -gamepad1.left_stick_y);
+        double translationX = Math.copySign(Math.pow(gamepad1.left_stick_x, 2), -gamepad1.left_stick_x);
+        double rotation = Math.copySign(Math.pow(gamepad1.right_stick_x, 2), gamepad1.right_stick_x);
+
+        drivetrainSubsystem.drive(translationY, translationX, rotation);
+
+        if (gamepad1.backWasPressed()) {
+            drivetrainSubsystem.resetYaw();
+        }
+
+        if (gamepad2.aWasPressed()) {
+            spinFlywheel = true;
+        }
+        if (gamepad2.bWasPressed()) {
+            spinFlywheel = false;
+        }
+        if (spinFlywheel) {
+            flywheelSubsystem.run(0.6);
+        } else {
+            flywheelSubsystem.stop();
+        }
+
+        if (gamepad2.x) {
+            indexerSubsystem.run(1);
+        } else {
+            indexerSubsystem.run(0);
+        }
+    }
+
+    @Override
+    public void stop() {
+        drivetrainSubsystem.stop();
+    }
+
 }
